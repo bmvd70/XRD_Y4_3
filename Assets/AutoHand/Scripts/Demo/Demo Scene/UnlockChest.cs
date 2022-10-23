b202cac98cff4e68a2d9b91bcd39650a0262789c
XRLAB1 <299123@via.dk>
2022-10-23 18:22:03 +0200

    Buttons work
    Co-Authored-By: luciacp1312 <80093487+luciacp1312@users.noreply.github.com>

--- Assets/AutoHand/Scripts/Demo/Demo Scene/UnlockChest.cs
+++ Assets/AutoHand/Scripts/Demo/Demo Scene/UnlockChest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,18 +6,53 @@ using UnityEngine;
 public class UnlockChest : MonoBehaviour
 {
     public HingeJoint joint;
-
+    public int count1, count2, count3 ;
     public void Unlock() {
-        joint.limits = new JointLimits {
-            bounceMinVelocity = joint.limits.bounceMinVelocity,
-            bounciness = joint.limits.bounciness,
-            contactDistance = joint.limits.contactDistance,
-            min = 0,
-            max = 160
-        };
-        joint.spring = new JointSpring() { spring = 5, targetPosition = 160 };
+        
+        Debug.Log("entered");
+            joint.limits = new JointLimits {
+                bounceMinVelocity = joint.limits.bounceMinVelocity,
+                bounciness = joint.limits.bounciness,
+                contactDistance = joint.limits.contactDistance,
+                min = 0,
+                max = 160
+            };
+            joint.spring = new JointSpring() { spring = 5, targetPosition = 160 };
+        
+       
     }
 
+    public void increaseCount1()
+    {
+        count1++;
+      ButtonsClicked();
+        Debug.Log("count1 " + count1);
+    }
+    public void increaseCount2()
+    {
+        count2++;
+        ButtonsClicked();
+
+        Debug.Log("count2 " + count2);
+    }
+    public void increaseCount3()
+    {
+        count3++;
+        ButtonsClicked();
+
+        Debug.Log("count3 " + count3);
+    }
+
+    public void ButtonsClicked()
+    {
+        if (count1 >= 1 && count2 >= 1 && count3 >= 1)
+        {
+            Unlock();
+            Debug.Log("All buttons clicked " );
+
+        }
+
+    }
     public void Lock() {
         joint.limits = new JointLimits {
             bounceMinVelocity = joint.limits.bounceMinVelocity,
